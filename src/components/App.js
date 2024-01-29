ed688558fef17d4a7a7e464f0f3385bea2010f06
Naomi Lagat <naomiejerutoe@gmail.com>
2024-01-29 22:46:38 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,23 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+function About() {
+  /* write an <About> component here */
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home />
       {/* add your <About> component here */}
+      <About />
     </div>
   );
 }
